3d93539f664bd497ca7803e928148f99b75f5c68
Rolix <yongandwise@yahoo.com>
2011-07-07 17:03:26 +0000

    added view

--- ticket/eticket/views.py
+++ ticket/eticket/views.py
@@ -5,25 +5,29 @@ from django.template import Context, loader
 from django.forms import ModelForm
 from django.views.decorators.csrf import csrf_exempt
 
-def site_list(request):
+def home(request):
 	t = loader.get_template('eticket/index.html')
 	c = Context(dict())
 	return HttpResponse(t.render(c))
 
-def booking _details(request):
-	return
 
-def route_detail(request):
-	return
+#def site_list(request):
+	#t = loader.get_template('eticket/index.html')
+	#c = Context(dict())
+	#return HttpResponse(t.render(c))
 
-def purchase_detail(request):
-	return 
+#def booking _details(request):
+	#def route_detail(request):
+	#return
 
-def cancell_ticket(request):
-	return
+#def purchase_detail(request):
+	#return 
 
-def events(request):
-	return
+#def cancell_ticket(request):
+	#return
 
-def event_detail():
-	return
+#def events(request):
+	#return
+
+#def event_detail():
+	#return
