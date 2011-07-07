a64cf2f48c680c048ffde6bb39c66fcecfcd10a1
Rolix <yongandwise@yahoo.com>
2011-07-07 14:23:29 +0000

    another model added

--- ticket/eticket/models.py
+++ ticket/eticket/models.py
@@ -1,12 +1,23 @@
 from django.db import models
+from django.contrib import admin
 
 class Booking(models.Model):
 	ticketType = models.CharField(max_length=30)
 	ticketQuantity = models.IntegerField()
-	ticketNum = models.IntegerField()
+	#ticketNum = models.IntegerField()
 	seatNum = models.IntegerField()
 	def __unicode__(self):
-		pass
+		return self.ticketType
+
+class Ticket(models.Model):
+	ticketNum = models.IntegerField()
+	booking = models.ForeignKey(Booking)	
+
+class TicketInline(admin.TabularInline):
+	model = Ticket
+
+class BookingAdmin(admin.ModelAdmin):
+	inlines=[TicketInline]
 
 class Customer(models.Model):
 	fName = models.CharField(max_length=20)
@@ -22,14 +33,14 @@ class Company(models.Model):
 	address = models.CharField(max_length=255)
 	email = models.EmailField()
 	ticketNum = models.ForeignKey(Booking)
- 	def __unicode__(self):
+	def __unicode__(self):
 		return self.name
 
 class Route(models.Model):
 	companyName = models.CharField(max_length=50)
 	departTime = models.DateTimeField()
 	origin = models.CharField(max_length=30)
-	destination = models.Charfield(max_length=30)
+	destination = models.CharField(max_length=30)
 	price = models.DecimalField(max_digits=4, decimal_places=2)
 	ticketLeft = models.IntegerField()
 	totalTickets = models.IntegerField()
@@ -44,3 +55,6 @@ class CompanyAdmin(models.Model):
 	sName = models.CharField(max_length=25)
 	def __unicode__(self):
 		pass
+
+admin.site.register(Booking, BookingAdmin)
+admin.site.register(Ticket)
